0a5d1834e3ecdcca07af0d0a8935a5d62f8c8e31
NilasiJayathilake <136611590+NilasiJayathilake@users.noreply.github.com>
2026-04-20 10:09:50 +0530

    initial commit

---
+++ src/main/java/com/example/m2nc/M2NcApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2nc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2NcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2NcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m2nc/M2NcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2nc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2NcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
